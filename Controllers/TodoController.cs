4c3fc51083a8957bc5c51ed2869cbeebbd15cac3
zylch11 <waqas.ahmed11@protonmail.com>
2020-06-07 17:17:10 +0500

    Service code done

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
 
 namespace AspNetCoreTodo.Controllers 
 {
     public class TodoController : Controller
     {
-        public IActionResult Index() 
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
         {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index() 
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
             // Get todo items from the database
 
             // Put the items into a model
--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,9 @@ namespace AspNetCoreTodo
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
            services.AddRazorPages();
+           services.AddMvc();
+
+           services.AddSingleton<ITodoItemService, FakeTodoItemService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
