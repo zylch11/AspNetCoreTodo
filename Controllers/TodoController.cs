56f63d9a1a4ee8994c98d604b55697fd655b8f61
zylch11 <waqas.ahmed11@protonmail.com>
2020-06-07 09:18:14 +0500

    TodoController, TodoItem & ViewModel done

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers 
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index() 
+        {
+            // Get todo items from the database
+
+            // Put the items into a model
+
+            // Render view using the model
+        }
+    }
+}
