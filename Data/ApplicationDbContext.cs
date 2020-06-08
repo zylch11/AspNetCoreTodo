40f427c4e61dbc3815169f65ca4aff473f1b6eb7
zylch11 <waqas.ahmed11@protonmail.com>
2020-06-08 09:17:07 +0500

    Migration created, Database connection complete

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Data
 {
@@ -12,5 +13,7 @@ namespace AspNetCoreTodo.Data
             : base(options)
         {
         }
+
+        public DbSet<TodoItem> Items { get; set; }
     }
 }
